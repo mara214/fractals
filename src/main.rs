59cf0240959b6dda813e2a2dbade9381ab9e724a
Maximilian Schulke <hello@maximilianschulke.com>
2021-09-24 00:02:28 +0200

    Fix output filename

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,5 @@ const HEIGHT: u32 = 4096;
 fn main() -> image::ImageResult<()> {
     let set = JuliaSet::new(WIDTH, HEIGHT, PI * 0.25);
     let img = ImageBuffer::from_fn(WIDTH, HEIGHT, |x, y| set.render(x, y));
-    img.save("dist/fractal.png")
+    img.save("dist/julia.png")
 }
